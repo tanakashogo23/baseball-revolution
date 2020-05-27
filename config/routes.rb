a8cccc88eae4f2ab51742b60f6b08e10691c8044
tanakashogo23 <shogo.shiori@icloud.com>
2020-05-28 00:20:47 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  resources :posts, only: [:new, :create]
+  root to: "posts#index"
+  resources :posts, only: [:index, :new, :create] do
+    collection do
+      get 'rule'
+    end
+  end
 end
