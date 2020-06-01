7efc82513278ebcc7eeb96d0541abd9d8a097455
tanakashogo23 <shogo.shiori@icloud.com>
2020-06-01 23:01:22 +0900

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts, only: [:index, :new, :create, :show] do
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     collection do
       get 'rule'
     end
